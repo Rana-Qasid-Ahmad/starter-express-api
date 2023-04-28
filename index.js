f0d63f3260d05b220a09670a40758ec3656ebd4e
Rana <zrana1791@gmail.com>
2023-04-29 00:21:28 +0500

    get request added

--- index.js
+++ index.js
@@ -11,10 +11,19 @@ dbConnect()
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.post('/', async(req, res) => {
-        const { title, content } = req.body;
+    res.sendFile(`${filesPath}/register.html`)
+    const { title, content } = req.body;
+    try {
+        const newPost = await postModel.create({ title, content });
+        res.json(newPost);
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+app.get('/', async(req, res) => {
         try {
-            const newPost = await postModel.create({ title, content });
-            res.json(newPost);
+            const post = await postModel.find();
+            res.json(post);
         } catch (error) {
             res.status(500).send(error)
         }

9d18df367f1d2d73d1c1a7e9c0b550b5efc08ab7
Rana <zrana1791@gmail.com>
2023-04-28 17:15:47 +0500

    changed 1

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const path = require('path');
+const filesPath = path.join(__dirname, "files");
+
+app.get('/', (req, resp) => {
+    resp.sendFile(`${filesPath}/index.html`)
 })
+
+
+
+
 app.listen(process.env.PORT || 3000)
